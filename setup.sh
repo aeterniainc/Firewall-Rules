45beedb595830a3d74e931ae637abe79c6665e7c
ㅤㅤㅤㅤ <37308627+Reflyansyah@users.noreply.github.com>
2019-12-15 15:41:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
-
+echo "Allow Port 22 & First Configuration"
+iptables -P FORWARD DROP
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -s 127.0.0.0/8 -d 127.0.0.0/8 -i lo -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
